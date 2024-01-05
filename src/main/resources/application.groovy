42d74957172dd464968d62d1dd619289440a43dd
Smit <smitc177@gmail.com>
2024-01-05 15:35:07 +0530

    Initial commit

---
+++ src/main/resources/application.groovy
@@ -0,0 +1,10 @@
+server {
+    port = 8080
+}
+
+spring.datasource {
+    url = "jdbc:postgresql://localhost:5432/mydatabase"
+    username = "myuser"
+    password = "mypassword"
+    driverClassName = "org.postgresql.Driver"
+}
---
+++ src/test/java/com/example/transactionmanager/TransactionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transactionmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
